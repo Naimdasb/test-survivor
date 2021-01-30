38d8b5cace478cbbf4724ac5ab4877a906449c28
naimdasb <naimdasb@gmail.com>
2021-01-30 17:17:00 -0300

    add rules form

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -19,35 +19,48 @@ export const Form = ({ onFinish }: Props) => {
                 <Item
                     label="Name"
                     name="name"
-                    rules={[{ required: true, message: "Please enter survivor's name." }]}
+                    rules={[{ required: true, message: "Please enter survivor's name." },
+                    { max: 20, message: "Max lenght is 20 characters." },
+                    ]}
                 >
                     <Input />
                 </Item>
                 <Item
                     label="Age"
                     name="age"
-                    rules={[{ required: true, message: "Please enter survivor's weight." }]}
+                    rules={[{ required: true, message: "Please enter survivor's weight." },
+                    { max: 3, message: "Max lenght is 3 characters." },
+                    { type: "number", message: "Numbers only." }
+                    ]}
                 >
                     <Input />
                 </Item>
                 <Item
                     label="City"
                     name="city"
-                    rules={[{ required: true, message: "Please enter survivor's city." }]}
+                    rules={[{ required: true, message: "Please enter survivor's city." },
+                    { max: 20, message: "Max lenght is 20 characters." }
+                    ]}
                 >
                     <Input />
                 </Item>
                 <Item
-                    label="Weight"
+                    label="Weight (kg)"
                     name="weight"
-                    rules={[{ required: true, message: "Please enter survivor's weight." }]}
+                    rules={[{ required: true, message: "Please enter survivor's weight." },
+                    { max: 3, message: "Max lenght is 3 characters." },
+                    { type: "number", message: "Numbers only." }
+                    ]}
                 >
                     <Input />
                 </Item>
                 <Item
-                    label="Height"
+                    label="Height (cm)"
                     name="height"
-                    rules={[{ required: true, message: "Please enter survivor's height." }]}
+                    rules={[{ required: true, message: "Please enter survivor's height." },
+                    { max: 3, message: "Max lenght is 3 characters." },
+                    { type: "number", message: "Numbers only." }
+                    ]}
                 >
                     <Input />
                 </Item>
